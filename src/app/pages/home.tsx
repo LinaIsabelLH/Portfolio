4eecf3ece571c555dc27bdf6666ce71f33d2edb5
LinaIsabelLH <londonohidalgo@gmail.com>
2025-02-13 18:57:06 +0100

    organisation projet

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../components/Header";
-import Section from "../components/Section";
+import Header from "../../components/Header";
+import Section from "../../components/Section";
 import Image from "next/image";
 
 const Home = () => {
