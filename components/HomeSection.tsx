9674cb8170aa7611a7eb341d3b1e39e1b7a0ff99
LinaIsabelLH <londonohidalgo@gmail.com>
2025-02-23 19:15:44 +0100

    revue mise en page Accueil et correction animation ProfileSection

--- components/HomeSection.tsx
+++ components/HomeSection.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 const HomeSection = () => {
+
+  const scrollToNextSection = () =>{
+    const nextSection = document.getElementById("profil");
+    if(nextSection){
+      nextSection.scrollIntoView({behavior:"smooth"});
+    }
+  }
   return (
     <section
       id="accueil"
@@ -9,13 +16,16 @@ const HomeSection = () => {
       {/* Overlay semi-transparent */}
       <div className="absolute inset-0 bg-slate-800/40"></div>
       <div className="z-10 font-body text-zinc-100">
-        <h2 className="font-extrabold text-5xl mt-16">
+        <h2 className="font-extrabold text-5xl mt-60">
           Bonjour et Bienvenu(e)!
         </h2>
-        <p className="font-medium text-3xl mt-11">
-          Je suis <span className="font-bold">Lina Isabel Londono Hidalgo</span>
-        </p>
-        <p className="font-medium text-2xl ">Développeuse Fullstack</p>
+        <h3 className="font-medium text-2xl mt-11">
+          Je suis <span className="font-bold text-3xl">Lina Isabel, Développeuse Fullstack</span> en pleine évolution.
+        </h3>
+        <p className="font-medium text-2xl mt-2"> J&apos;adore coder, apprendre et créer des applications modernes!</p>
+        <p className="mt-16 text-2xl font-medium ">Curieux(se)?</p>
+        <p className="mt-2 text-2xl font-medium ">Découvre mon profil et mes realisations!</p>
+        <p className="mt-2 text-3xl animate-bounce cursor-pointer" onClick={()=>scrollToNextSection()}>👇</p>
       </div>
     </section>
   );
--- components/ProfileSection.tsx
+++ components/ProfileSection.tsx
@@ -3,112 +3,64 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import GithubLogo from "./svg/GithubLogo";
 import LinkedinLogo from "./svg/LinkedinLogo";
+import styles from "../styles/home.module.css";
 
 const ProfileSection = () => {
   return (
     <>
       <div className="z-10 flex justify-center items-center relative">
         {/* Conteneur du texte avec la bordure animée */}
-        <div className="relative mt-20 max-w-xl my-4  bg-slate-800 text-zinc-100 p-6 rounded-4xl overflow-clip">
-          {/* Bordures animées */}
-          {/* Gauche (début de l'animation) */}
-          <motion.div
-            className="absolute top-0 left-0 w-[2px] h-0 bg-orange-400"
-            style={{ borderRadius: "4rem" }} // Coins arrondis
-            initial={{ height: 0 }}
-            animate={{ height: "100%" }}
-            transition={{ duration: 1, ease: "easeInOut" }}
-          />
-          {/* Haut */}
-          <motion.div
-            className="absolute top-0 left-0 w-0 h-[2px] bg-orange-400"
-            style={{ borderRadius: "4rem" }} // Coins arrondis
-            initial={{ width: 0 }}
-            animate={{ width: "100%" }}
-            transition={{ duration: 2, ease: "easeInOut", delay: 2 }}
-          />
-          {/* Bas */}
-          <motion.div
-            className="absolute bottom-0 left-0 w-0 h-[2px] bg-orange-400"
-            style={{ clipPath: "inset(0 round 5rem)" }} // Coins arrondis
-            initial={{ width: 0 }}
-            animate={{ width: "100%" }}
-            transition={{ duration: 2, ease: "easeInOut", delay: 2 }}
-          />
-          {/* Droite */}
-          {/* Partie haute */}
-          <motion.div
-            className="absolute top-0 right-0 w-[2px] h-0 bg-orange-400"
-            style={{ clipPath: "inset(0 round 5rem)" }}
-            initial={{ height: 0 }}
-            animate={{ height: "50%" }} // Grandit vers le bas
-            transition={{ duration: 3, ease: "easeInOut", delay: 4 }}
-          />
-
-          {/* Partie basse */}
-          <motion.div
-            className="absolute bottom-0 right-0 w-[2px] h-0 bg-orange-400"
-            style={{ clipPath: "inset(0 round 5rem)" }}
-            initial={{ height: 0 }}
-            animate={{ height: "50%" }} // Grandit vers le haut
-            transition={{ duration: 3, ease: "easeInOut", delay: 4 }}
-          />
-          {/* Droite (fin de l'animation, vers la photo) */}
-          <motion.div
-            className="absolute top-1/2 right-0 w-[2px] h-0 bg-orange-400"
-            initial={{ width: 0 }}
-            animate={{ width: "100%" }}
-            transition={{ duration: 4, ease: "easeInOut", delay: 5 }}
-          />
-
+        <div className={`relative mt-20 max-w-xl my-4 p-1 ${styles.homeDescriptionContainer}`}>
           {/* Contenu du texte */}
-          <p>
-            Je suis une{" "}
-            <span className="font-bold text-orange-400">
-              Développeuse Fullstack
-            </span>{" "}
-            passionnée par la{" "}
-            <span className="font-bold">
-              création d&apos;applications web et mobiles
-            </span>{" "}
-            innovantes qui apportent une réelle valeur aux entreprises.
-            Originaire de Colombie, j’ai choisi la France il y a 10 ans pour y
-            étudier, et j’y ai construit ma carrière et ma vie.
-          </p>
-          <p className="py-3">
-            Après un parcours riche dans le commerce et la banque, où j’ai
-            développé mon <span className="font-bold">adaptabilité </span>et mon{" "}
-            <span className="font-bold">sens relationnel</span>, j’ai décidé en
-            2024 de me consacrer pleinement au Développement Fullstack, séduite
-            par son potentiel d’innovation et d’
-            <span className="font-bold">apprentissage continu</span>.
-          </p>
-          <p>
-            <span className="font-bold">
-              Curieuse, rigoureuse et avide de défis
-            </span>
-            , j’aime transformer les idées en solutions concrètes et
-            performantes, en explorant sans cesse de nouvelles technologies.
-          </p>
-          <div className="flex justify-center">
-            <a
-              href="https://www.linkedin.com/in/lina-isabel-londono-hidalgo/"
-              target="_blank"
-              aria-label="Linkedin link"
-              rel="noreferrer"
-              className="my-4"
-            >
-              <LinkedinLogo />
-            </a>
-            <a
-              href="https://github.com/LinaIsabelLH"
-              target="_blank"
-              aria-label="Github link"
-              rel="noreferrer"
-              className="my-4 ml-10"
-            >
-              <GithubLogo />
-            </a>
+          <div className="relative bg-slate-800 text-zinc-100 p-6 rounded-4xl ">
+            <p>
+              Je suis une{" "}
+              <span className="font-bold text-orange-400">
+                Développeuse Fullstack
+              </span>{" "}
+              passionnée par la{" "}
+              <span className="font-bold">
+                création d&apos;applications web et mobiles
+              </span>{" "}
+              innovantes qui apportent une réelle valeur aux entreprises.
+              Originaire de Colombie, j’ai choisi la France il y a 10 ans pour y
+              étudier, et j’y ai construit ma carrière et ma vie.
+            </p>
+            <p className="py-3">
+              Après un parcours riche dans le commerce et la banque, où j’ai
+              développé mon <span className="font-bold">adaptabilité </span>et
+              mon <span className="font-bold">sens relationnel</span>, j’ai
+              décidé en 2024 de me consacrer pleinement au Développement
+              Fullstack, séduite par son potentiel d’innovation et d’
+              <span className="font-bold">apprentissage continu</span>.
+            </p>
+            <p>
+              <span className="font-bold">
+                Curieuse, rigoureuse et avide de défis
+              </span>
+              , j’aime transformer les idées en solutions concrètes et
+              performantes, en explorant sans cesse de nouvelles technologies.
+            </p>
+            <div className="flex justify-center">
+              <a
+                href="https://www.linkedin.com/in/lina-isabel-londono-hidalgo/"
+                target="_blank"
+                aria-label="Linkedin link"
+                rel="noreferrer"
+                className="my-4"
+              >
+                <LinkedinLogo />
+              </a>
+              <a
+                href="https://github.com/LinaIsabelLH"
+                target="_blank"
+                aria-label="Github link"
+                rel="noreferrer"
+                className="my-4 ml-10"
+              >
+                <GithubLogo />
+              </a>
+            </div>
           </div>
         </div>
 
@@ -119,7 +71,7 @@ const ProfileSection = () => {
             className="absolute left-[-96px] top-4/7 w-[30px] h-[2px] bg-orange-400"
             initial={{ width: 0 }}
             animate={{ width: "100px" }}
-            transition={{ duration: 1, ease: "easeInOut", delay: 7 }}
+            transition={{ duration: 2, ease: "easeInOut", delay: 2 }}
           />
 
           <div className="w-80 h-100 overflow-hidden rounded-[50%] border-2 border-orange-400">
@@ -135,8 +87,8 @@ const ProfileSection = () => {
         </div>
       </div>
 
-      <div className="z-10">
-        <button className="font-bold text-zinc-100 ">En savoir plus👇</button>
+      <div className="z-10 mt-16">
+        <button className="font-bold text-zinc-100 text-xl">En savoir plus...</button>
       </div>
     </>
   );
