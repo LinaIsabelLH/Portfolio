28dad57365392081eadc9db2775a9672981e4073
LinaIsabelLH <londonohidalgo@gmail.com>
2025-03-16 17:38:19 +0100

    competences section en responsive OK

--- components/competences/CompetencesSection.tsx
+++ components/competences/CompetencesSection.tsx
@@ -20,8 +20,8 @@ const CompetencesSection = () => {
           data-aos-duration="1000"
           data-aos-easing="ease-in-out"
         >
-          <div className="relative flex flex-col mb-10 pt-5 bg-gray-800 shadow-orange-300/50 shadow-lg border border-orange-400 rounded-4xl max-[1263px]:mx-4">
-            <h3 className="text-orange-400 font-bold text-2xl text-center px-[10px] py-0 max-[800px]:text-xl">
+          <div className="relative flex flex-col mb-10 pt-5 bg-gray-800 shadow-orange-300/50 shadow-lg border border-orange-400 rounded-4xl max-[1263px]:mx-4 max-[709px]:mx-2 max-[709px]:mb-6 max-[540px]:pt-[5px] max-[425px]:rounded-2xl">
+            <h3 className="text-orange-400 font-bold text-2xl text-center px-[10px] py-0 max-[800px]:text-xl max-[540px]:text-base max-[540px]:px-[4px]">
               FrontEnd
             </h3>
             <CompetencesStack competences={frontCompetences} />
@@ -36,8 +36,8 @@ const CompetencesSection = () => {
           data-aos-duration="1000"
           data-aos-easing="ease-in-out"
         >
-          <div className="relative flex flex-col mb-10 pt-5 bg-gray-800 shadow-orange-300/50 shadow-lg border border-orange-400 rounded-4xl max-[1263px]:mx-2">
-            <h3 className="text-orange-400 font-bold text-2xl text-center px-[10px] py-0 max-[800px]:text-xl">
+          <div className="relative flex flex-col mb-10 pt-5 bg-gray-800 shadow-orange-300/50 shadow-lg border border-orange-400 rounded-4xl max-[1263px]:mx-2 max-[709px]:mb-6  max-[540px]:pt-[5px] max-[425px]:rounded-2xl">
+            <h3 className="text-orange-400 font-bold text-2xl text-center px-[10px] py-0 max-[800px]:text-xl max-[540px]:text-base">
               BackEnd
             </h3>
             <CompetencesStack competences={backCompetences} />
@@ -51,15 +51,15 @@ const CompetencesSection = () => {
           data-aos-duration="1000"
           data-aos-easing="ease-in-out"
         >
-          <div className="relative flex flex-col mb-10 pt-5 bg-gray-800 shadow-orange-300/50 shadow-lg border border-orange-400 rounded-4xl max-[1263px]:ml-2 max-[1263px]:mr-4">
-            <h3 className="text-orange-400 font-bold text-2xl text-center px-[10px] py-0 max-[800px]:text-xl">
+          <div className="relative flex flex-col mb-10 pt-5 bg-gray-800 shadow-orange-300/50 shadow-lg border border-orange-400 rounded-4xl max-[1263px]:ml-2 max-[1263px]:mr-4 max-[709px]:mx-2 max-[709px]:mb-6 max-[540px]:pt-[5px] max-[425px]:rounded-2xl">
+            <h3 className="text-orange-400 font-bold text-2xl text-center px-[10px] py-0 max-[800px]:text-xl max-[540px]:text-base ">
               Les outils que j&apos;utilise
             </h3>
             <CompetencesStack competences={toolsCompetences} />
           </div>
         </div>
       </div>
-      <div className="flex flex-col max-h-56">
+      <div className="flex flex-col max-h-56 max-[430px]:max-w-[95%] max-[430px]:mx-auto">
         <div
           className="m-auto flex flex-col"
           data-aos="fade-up"
@@ -68,8 +68,8 @@ const CompetencesSection = () => {
           data-aos-duration="1000"
           data-aos-easing="ease-in-out"
         >
-          <div className="relative flex flex-col mb-10 pt-5 bg-gray-800 shadow-orange-300/50 shadow-lg border border-orange-400 rounded-4xl max-[1263px]:mx-4 max-[800px]:mb-2">
-            <h3 className="text-orange-400 font-bold text-2xl text-center px-[10px] py-0 max-[800px]:text-xl">
+          <div className="relative flex flex-col mb-10 pt-5 pb-2 bg-gray-800 shadow-orange-300/50 shadow-lg border border-orange-400 rounded-4xl max-[1263px]:mx-4 max-[709px]:mx-2 max-[800px]:mb-4 max-[540px]:pt-[5px] max-[540px]:mb-[4px] max-[540px]:mx-[6px] max-[425px]:pb-0 max-[425px]:rounded-2xl">
+            <h3 className="text-orange-400 font-bold text-2xl text-center px-[10px] py-0 max-[800px]:text-xl max-[540px]:text-base max-[540px]:pt-[4px]">
               Les technos que j&apos;ai travaillé
             </h3>
             <CompetencesStack competences={technoCompetences} />
--- components/competences/CompetencesStack.tsx
+++ components/competences/CompetencesStack.tsx
@@ -14,18 +14,18 @@ const CompetencesStack = (props: Props): JSX.Element => {
     return competences.map((competence) => {
       if (competence.svg) {
         return (
-          <div key={competence.name} className='flex flex-col justify-center items-center gap-4 max-[768px]:gap-2  max-[768px]:px-2.5 max-[768px]:py-2.5'>
-            <div role="img" aria-label={competence.name} className="z-10 bg-[radial-gradient(#1e293b,#52525b)] w-[85px] h-[85px] flex items-center justify-center rounded-full border-2 border-gray-800 drop-shadow-xl box-border max-[800px]:w-[50px] max-[800px]:h-[50px]">
+          <div key={competence.name} className='flex flex-col justify-center items-center gap-4 max-[768px]:gap-2  max-[768px]:px-2.5 max-[768px]:py-2.5 max-[425px]:px-[7px] max-[425px]:py-[7px]'>
+            <div role="img" aria-label={competence.name} className="z-10 bg-[radial-gradient(#1e293b,#52525b)] w-[85px] h-[85px] flex items-center justify-center rounded-full border-2 border-gray-800 drop-shadow-xl box-border max-[800px]:w-[50px] max-[800px]:h-[50px] max-[476px]:w-[35px] max-[476px]:h-[35px]">
               {competence.svg}
             </div>
-            <h4 className='text-zinc-100 font-semibold text-xl max-[800px]:text-lg'>{competence.name}</h4>
+            <h4 className='text-zinc-100 font-semibold text-xl max-[800px]:text-base max-[540px]:text-sm'>{competence.name}</h4>
           </div>
         );
       }
 
       return (
-        <div key={competence.name} className='flex flex-col justify-center items-center gap-4 max-[768px]:gap-2 max-[768px]:px-2.5 max-[768px]:py-2.5'>
-          <div className="z-10 bg-[radial-gradient(#1e293b,#52525b)] w-[85px] h-[85px] flex items-center justify-center rounded-full border-2 border-gray-800 drop-shadow-xl box-border max-[768px]:w-20 max-[768px]:h-20 max-[800px]:w-[50px] max-[800px]:h-[50px]">
+        <div key={competence.name} className='flex flex-col justify-center items-center gap-4 max-[768px]:gap-2 max-[768px]:px-2.5 max-[768px]:py-2.5 max-[425px]:px-[7px] max-[425px]:py-[7px]'>
+          <div className="z-10 bg-[radial-gradient(#1e293b,#52525b)] w-[85px] h-[85px] flex items-center justify-center rounded-full border-2 border-gray-800 drop-shadow-xl box-border max-[800px]:w-[50px] max-[800px]:h-[50px] max-[476px]:w-[35px] max-[476px]:h-[35px]">
             <Image
               width={100}
               height={100}
@@ -35,14 +35,14 @@ const CompetencesStack = (props: Props): JSX.Element => {
               className='h-2/3 w-2/3 object-contain'
             />
           </div>
-          <h4 className='text-zinc-100 font-semibold text-xl max-[800px]:text-lg'>{competence.name}</h4>
+          <h4 className='text-zinc-100 font-semibold text-xl max-[800px]:text-base max-[540px]:text-sm '>{competence.name}</h4>
         </div>
       );
     });
   }, [competences]);
 
   return (
-    <div className='flex justify-center flex-wrap gap-y-8 gap-x-16 pt-[20px] pr-[30px] pl-[30px] pb-[30px] max-[970px]:gap-y-2 max-[970px]:gap-x-8 max-[800px]:p-3'>
+    <div className='flex justify-center flex-wrap gap-y-8 gap-x-16 pt-[20px] pr-[30px] pl-[30px] pb-[30px] max-[970px]:gap-y-2 max-[970px]:gap-x-8 max-[800px]:p-3 max-[768px]:gap-x-[4px] max-[709px]:px-0 max-[709px]:py-[4px] max-[540px]:gap-y-0 max-[540px]:gap-x-0'>
       {competencesList}
     </div>
   );
