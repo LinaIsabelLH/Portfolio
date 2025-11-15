93842989b5c4d1871b5f039384e1b30472b2795b
LinaIsabelLH <londonohidalgo@gmail.com>
2025-11-15 18:22:23 +0100

    translation in progress home and hero

--- i18n/locales.ts
+++ i18n/locales.ts
@@ -1,24 +1,79 @@
 export const locales = {
-	fr: {
-		header: {
-			home: "Accueil",
-			profil: "Profil",
-			skills: "Compétences",
-			portfolio: "Portfolio",
-			contact: "Contact"
-		},
-
+  fr: {
+    header: {
+      home: "Accueil",
+      profil: "Profil",
+      skills: "Compétences",
+      portfolio: "Portfolio",
+      contact: "Contact",
     },
+    home: {
+      greet: "Bonjour et Bienvenu(e)!",
+      intro: "Je suis",
+      intro1: "Lina Isabel, Développeuse Fullstack",
+      intro2: "en pleine évolution.",
+      intro3: "J'adore coder, apprendre et créer des applications modernes!",
+      intro4: "Curieux(se)?",
+      intro5: "Découvre mon profil et mes realisations!",
+    },
+    hero: {
+      title: "",
+      description: "Je suis une",
+      poste: "Développeuse Fullstack",
+      description1: "passionnée par la",
+      description2: "création d'applications web et mobiles",
+      description3:
+        "innovantes qui apportent une réelle valeur aux entreprises. Originaire de Colombie, j’ai choisi la France il y a 10 ans pour y étudier, et j’y ai construit ma carrière et ma vie.",
+      description4:
+        "Après un parcours riche dans le commerce et la banque, où j’ai développé mon",
+      description5: "adaptabilité",
+      description6: "et mon",
+      description7: "sens relationnel",
+      description8:
+        ", j’ai décidé en 2024 de me consacrer pleinement au Développement Fullstack, séduite par son potentiel d’innovation et d’",
+      description9: "apprentissage continu",
+      description10: "Curieuse, rigoureuse et avide de défis",
+      description11:
+        ", j’aime transformer les idées en solutions concrètes et performantes, en explorant sans cesse de nouvelles technologies.",
+    },
+  },
 
-
-
-    en: {
-		header: {
-			home: "Home",
-			profil: "Profile",
-			skills: "Skills",
-			portfolio: "Portfolio",
-			contact: "Contact"
-		},
-
-}}
+  en: {
+    header: {
+      home: "Home",
+      profil: "Profile",
+      skills: "Skills",
+      portfolio: "Portfolio",
+      contact: "Contact",
+    },
+    home: {
+      greet: "Bonjour et Bienvenu(e)!",
+      intro: "Je suis",
+      intro1: "Lina Isabel, Développeuse Fullstack",
+      intro2: "en pleine évolution.",
+      intro3: "J'adore coder, apprendre et créer des applications modernes!",
+      intro4: "Curieux(se)?",
+      intro5: "Découvre mon profil et mes realisations!",
+    },
+    hero: {
+      title: "",
+      description: "Je suis une",
+      poste: "Développeuse Fullstack",
+      description1: "passionnée par la",
+      description2: "création d'applications web et mobiles",
+      description3:
+        "innovantes qui apportent une réelle valeur aux entreprises. Originaire de Colombie, j’ai choisi la France il y a 10 ans pour y étudier, et j’y ai construit ma carrière et ma vie.",
+      description4:
+        "Après un parcours riche dans le commerce et la banque, où j’ai développé mon",
+      description5: "adaptabilité",
+      description6: "et mon",
+      description7: "sens relationnel",
+      description8:
+        ", j’ai décidé en 2024 de me consacrer pleinement au Développement Fullstack, séduite par son potentiel d’innovation et d’",
+      description9: "apprentissage continu",
+      description10: "Curieuse, rigoureuse et avide de défis",
+      description11:
+        ", j’aime transformer les idées en solutions concrètes et performantes, en explorant sans cesse de nouvelles technologies.",
+    },
+  },
+};
